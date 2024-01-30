58e983faa89e6b5192f7ebfcc62eabec48885b4b
Pulkit Singh <pulkitsingh01@gmail.com>
2024-01-30 21:16:41 +0530

    mouse controls

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { Canvas } from 'react-three-fiber';
 import './App.css';
 import Sphere from './modules/sphere/Sphere';
 import HoverCar from './modules/hover-car/HoverCar';
+import { OrbitControls } from '@react-three/drei';
 
 function App() {
 
   return (
     <div>
-      <Canvas>
+      <Canvas camera={{ position: [0, 0, 5] }}>
         <ambientLight intensity={0.5} />
         <directionalLight position={[10, 10, 5]} />
+        <OrbitControls />
+
         {/* <Sphere position={[0, 0, 0]} /> */}
         <HoverCar />
       </Canvas>
