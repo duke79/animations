2feff4e03e2396ee54aa06c991442b587ac72594
Pulkit Singh <pulkitsingh01@gmail.com>
2024-01-31 02:38:00 +0530

    ./animations

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "./animations"
+// })
+
+// // load env variables to get base url
+// process.env = Object.assign(process.env, loadEnv('production', process.cwd(), ''));
+
 export default defineConfig({
-  plugins: [react()],
-  base: "./animations/"
+  // ...
+  experimental: {
+    renderBuiltUrl(filename) {
+        // here we set the base url. You might have to change this for react:
+        return  '/animations/dist/' + filename;
+    }
+  }
 })
